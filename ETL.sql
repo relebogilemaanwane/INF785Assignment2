96c303c16e5a798c7500324bfb437a90b27d8c71
relebogile maanwane <83724498+relebogilemaanwane@users.noreply.github.com>
2023-05-30 18:29:08 +0200

    dimEmployee

--- ETL.sql
+++ ETL.sql
@@ -119,4 +119,16 @@ INNER JOIN AdventureWorks2019.Person.CountryRegion AS CR ON CR.CountryRegionCode
 WHERE CustomerStaging.RowNum = 1
 
 --DimEmployee
+INSERT INTO DimEmployee(EmployeeKey,FirstName,LastName,JobTitle, Department)
+SELECT DISTINCT P.BusinessEntityID AS EmployeeKey, P.FirstName, P.LastName, E.JobTitle, D.Name AS Department
+FROM AdventureWorks2019.Person.Person AS P
+INNER JOIN [AdventureWorks2019].[HumanResources].[Employee] AS E ON P.[BusinessEntityID] = E.[BusinessEntityID]
+INNER JOIN [AdventureWorks2019].[HumanResources].[EmployeeDepartmentHistory] AS EDH ON P.[BusinessEntityID] = EDH.[BusinessEntityID]
+INNER JOIN [AdventureWorks2019].[HumanResources].[Department] AS D ON D.DepartmentID = EDH.[DepartmentID]
+WHERE P.FirstName IS NOT NULL
+  AND P.LastName IS NOT NULL 
+  AND E.JobTitle IS NOT NULL
+  AND D.Name = 'Sales'
+  ORDER BY P.BusinessEntityID;
+  
 --FactSales
